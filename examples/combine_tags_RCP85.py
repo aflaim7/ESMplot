3b5b550a4b8182ad7f01ecb4ca27fa76aa4bb883
Andrew Flaim <aflaim@wustl.edu>
2025-08-28 15:16:20 -0500

    fix coordinate attributes

--- examples/combine_tags_RCP85.py
+++ examples/combine_tags_RCP85.py
@@ -6,7 +6,7 @@ from ESMplot.watertagging.combine_tagged_regions import (
 )
 
 # Import the monthly concatenated climatology of the water tagged time slice
-ds = xr.open_dataset('/RAID/datasets/f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004/archive/atm/hist/climatology/' \
+ds = xr.open_dataset('/RAID/datasets/f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004/products/' \
 'f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004.cam.h0.2105-2124_monthly_climatology_cat.nc')
 
 ## Combine regions
@@ -54,6 +54,15 @@ ds_combined = combine_regions_to_new_tag(
     keep_nonregion_vars=True # keep other non-region variables
 )
 
-## Write out netcdf
-ds_combined.to_netcdf('/RAID/datasets/f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004/archive/atm/hist/climatology/' \
+# Let's also set the coordinate variables equal to the reference dataset
+ref = xr.open_dataset('/RAID/datasets/f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004/products/f.e12.F_1850_CAM5.wiso.f19.0ka.002.watertags.2.cam.h0.0006-0025.climo_combReg.nc')
+ds_combined = ds_combined.assign_coords(
+    lat=xr.IndexVariable('lat', ref.lat.values, attrs=ds_combined['lat'].attrs),
+    lon=xr.IndexVariable('lon', ref.lon.values, attrs=ds_combined['lon'].attrs),
+)
+
+## Write out netcdf 
+# (note that you must remove the destination filename if it already exists. 
+#   paleonas scripts can't overwrite falkor files because the group permissions are different)
+ds_combined.to_netcdf('/RAID/datasets/f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004/products/' \
 'f.ie12.BRCP85C5CN.f19_g16.LME.004_2100watertags.004.cam.h0.2105-2124_monClim_combReg.nc')
