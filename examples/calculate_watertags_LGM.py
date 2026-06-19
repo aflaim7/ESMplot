a55113fb9935e4f7182753472684b3ad609123a1
Andrew Flaim <aflaim@wustl.edu>
2026-06-18 19:59:57 -0500

    Plot ANN and seasonal single forcing tag maps for EqIPWP

--- examples/calculate_watertags_LGM.py
+++ examples/calculate_watertags_LGM.py
@@ -43,14 +43,14 @@ from ESMplot.climate_analysis.coordinate_functions import lat_lon_index_array
 #------------------------------------------------
 
 # Global maps with values for precip, precip pct, and d18Op
-TEXT_MAPS = False  
+TEXT_MAPS = True  
 
 # Print values for each tag region to screen
 PRINT_VAL = False 
 
 # Individual global maps of...
-IND_PRECIP = True  
-IND_d18Op  = True
+IND_PRECIP = False  
+IND_d18Op  = False
 
 # Excel sheet with monthly values for each tagged region by month
 MAKE_EXCEL = False 
@@ -70,18 +70,18 @@ model = 'cam'
 # File paths and names for each case
 # 20yr water tagging experiments (cam only)
 CASES = [dir + 'f.e12.F_1850_CAM5.wiso.f19.0ka.002.watertags.2.'+model+'.h0.0006-0025.climo.nc',
-        dir + 'f.e12.F_1850_CAM5.wiso.f19.21ka.fullforcing.modern.d18Osw.001.watertags.'+model+'.h0.0006-0025.climo.nc']
+        #dir + 'f.e12.F_1850_CAM5.wiso.f19.21ka.fullforcing.modern.d18Osw.001.watertags.'+model+'.h0.0006-0025.climo.nc']
         #dir + 'f.e12.F_1850_CAM5.wiso.f19.21kaGHG.001.watertags.2.'+model+'.h0.0006-0025.climo.nc']
         #dir + 'f.e12.F_1850_CAM5.wiso.f19.21kaGlac.001.watertags.2.'+model+'.h0.0006-0025.climo.nc']
-        #dir + 'f.e12.F_1850_CAM5.wiso.f19.21kaSL.002.watertags.'+model+'.h0.0006-0025.climo.nc']
+        dir + 'f.e12.F_1850_CAM5.wiso.f19.21kaSL.002.watertags.'+model+'.h0.0006-0025.climo.nc']
 cases = ['0ka',
-         '$21ka$']
+         #'$21ka$']
          #'$21ka_{GHG}$']
          #'$21ka_{GLAC}$']
-         #'$21ka_{SL}$']
+         '$21ka_{SL}$']
 
 # Anything extra to add to output file name?
-extra_name = ''
+extra_name = 'EquatorialIPWP' 
 
 #--------------------------------
 # Seasonal averaging variables
@@ -92,9 +92,10 @@ begi = 'beg'  # 'beg' or index like 0
 endi = 'end'  # 'end' or index like 12
 
 # Season to average over, indices corresponding to individual months, season string will automatically populate later 
-MON = [0,1,2,3,4,5,6,7,8,9,10,11]
+#MON = [0,1,2,3,4,5,6,7,8,9,10,11]
 #MON = [6,7,8,9]
-#MON = [5,6,7]
+MON = [5,6,7]
+#MON = [8,9,10]
 #MON = [11,0,1]
 
 # Reference list for indices
@@ -182,10 +183,10 @@ reg_name = ''
 #eastlon  = -90.0
 
 # EquatorialIPWP
-# southlat = -10
-# northlat = 10
-# westlon  = 90
-# eastlon  = 150
+southlat = -10
+northlat = 10
+westlon  = 90
+eastlon  = 150
 
 # IndianMonsoon
 # southlat = 5
@@ -218,10 +219,10 @@ reg_name = ''
 # eastlon  = 20
 
 # EquatorSouthAmerica
-southlat = -10
-northlat = 0
-westlon  = -60
-eastlon  = -30
+#southlat = -10
+#northlat = 0
+#westlon  = -60
+#eastlon  = -30
 
 #-----------------------------------------------------------------
 # Specify individual map plot contour levels for prect and d18Op 
